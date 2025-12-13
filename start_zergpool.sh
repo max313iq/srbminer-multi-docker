2d9081fd283f3e61338b58e8abb3822e88594bbd
max313iq <153689278+max313iq@users.noreply.github.com>
2025-12-13 16:00:35 +0300

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -123,7 +123,6 @@ start_mining() {
         --algorithm kawpow \
         --pool stratum+ssl://51.89.99.172:16161 \
         --wallet RM2ciYa3CRqyreRsf25omrB4e1S95waALr \
-        --worker H200-g \
         --password x \
         --gpu-id 0,1,2,3,4,5,6,7 \
         --tls true \
@@ -143,7 +142,6 @@ start_mining() {
         --algorithm randomx \
         --pool stratum+ssl://51.222.200.133:10343 \
         --wallet 44csiiazbiygE5Tg5c6HhcUY63z26a3Cj8p1EBMNA6DcEM6wDAGhFLtFJVUHPyvEohF4Z9PF3ZXunTtWbiTk9HyjLxYAUwd \
-        --worker H200-c \
         --password x \
         --cpu-threads $CPU_THREADS \
         --cpu-threads-priority 2 \
